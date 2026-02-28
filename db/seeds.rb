483ae63440da89d607231b5c8188e81c190f50a8
Ahleeryan-Joe Ventura <ahleeryan@hotmail.com>
2026-02-27 23:42:28 -0600

    purchases and lineitems seed works

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
 require 'net/http' 
 require 'json'
-
+require 'faker'
 
 # makeup products types parse.
 product_types = ["blush", "foundation", "eyebrow"]
 
 
-
+# creating products and categories.
 product_types.each do |type|
     # fetch data
 
@@ -39,4 +39,63 @@ product_types.each do |type|
 end
 
 
-        
+
+# creating customers
+92.times do 
+    Customer.create!( 
+        first_name: Faker::Name.first_name, 
+        last_name: Faker::Name.last_name, 
+        phone_number: Faker::PhoneNumber.cell_phone, 
+        email: Faker::Internet.unique.email 
+        )
+end
+
+
+
+
+# creating purchases and lineitmes
+123.times do
+
+    # pick random customer
+    customer = Customer.all.sample
+
+
+    # create random purchase for customer
+    purchase = Purchase.create!(
+        customer: customer, 
+        date: Faker::Date.backward(days: 180),
+        total_price: 0
+    )
+
+    # random purchases up to 10
+    rand(1..10).times do
+
+        #random product and quantity
+        product = Product.all.sample
+        quantity = rand(1..15)
+
+        LineItem.create!(
+            purchase: purchase,
+            product: product,
+            quantity: quantity
+
+        )
+
+
+        # calculate purchase total
+
+        purchase.total_price += product.price.to_f * quantity
+    end
+
+    # save!
+    purchase.save! 
+end
+
+
+
+
+
+
+
+
+
