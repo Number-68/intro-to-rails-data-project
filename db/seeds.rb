2ffa1fb4da4356f91cf33ba9e93e935f1e7f2d87
Ahleeryan-Joe Ventura <ahleeryan@hotmail.com>
2026-02-27 23:11:46 -0600

    seed.rb working api parse and set to product and category tables

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,42 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'net/http' 
+require 'json'
+
+
+# makeup products types parse.
+product_types = ["blush", "foundation", "eyebrow"]
+
+
+
+product_types.each do |type|
+    # fetch data
+
+    url = URI("http://makeup-api.herokuapp.com/api/v1/products.json?product_type=#{type}") 
+    response = Net::HTTP.get(url) 
+    items = JSON.parse(response)
+
+    # parse through data
+    items.each do |item|
+
+        # minimum price 1.00
+        price = item["price"].to_f
+
+        next if price < 1.0
+
+        # create or reuse category
+        category = Category.find_or_create_by!(
+            product_category: item["product_type"]
+        )
+
+
+        # create the row
+        Product.create!( 
+            brand: item["brand"], 
+            name: item["name"], 
+            price: price, 
+            category: category 
+            )
+    end
+end
+
+
+        
